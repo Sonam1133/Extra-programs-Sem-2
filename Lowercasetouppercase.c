8b610f57d74873d2de5ee81d3a901505fc60c640
Sonam1133 <146869916+Sonam1133@users.noreply.github.com>
2024-02-12 23:02:04 +0530

    Update Lowercasetouppercase.c

--- Lowercasetouppercase.c
+++ Lowercasetouppercase.c
@@ -1,1 +1,10 @@
+//lowercase to uppercase using bitwise 
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter the character");
+    scanf("%c",&ch);
+    ch=ch&223;
+    printf("%c",ch);
 
+}
