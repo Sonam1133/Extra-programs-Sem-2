d4db5d0321d8c25520cf7d58aff3892e4d4b9cfc
Sonam1133 <146869916+Sonam1133@users.noreply.github.com>
2024-03-03 23:26:20 +0530

    Add files via upload

---
+++ minimumelementofanarray.c
@@ -0,0 +1,26 @@
+// Minimum Element of an array
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of an array : ");
+    scanf("%d",&n);
+    
+    printf("The Elements of array are..\n");
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int min = arr[0];
+    
+    int index;
+
+    for(int i=0;i<=n-1;i++) {
+        if(arr[i]<min){
+            min = arr[i];
+            index=i;
+        }
+    }
+    printf("The minimum element is %d at index %d ",min,index);
+}
+
+
