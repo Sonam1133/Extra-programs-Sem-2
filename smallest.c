241689d82026dfd0f3752e893027ee8a7c2d6a92
Sonam1133 <146869916+Sonam1133@users.noreply.github.com>
2024-03-03 23:09:37 +0530

    Add files via upload

---
+++ smallest.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[10],sum=0;
+    int small=0,pos=0;
+    printf("Enter no. of elements in array: ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("a[%d]=",i);
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        small=a[0];
+        if(a[i]<small)
+        {
+            small=a[i];
+            pos=i;
+        }
+    }
+    printf("Smallest no:%d \n",small);
+    printf("Position:%d",pos);
+
+}
